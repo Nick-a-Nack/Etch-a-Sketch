616ed680b7e068f2ad9e44bc4be24515f2b42f66
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-06-16 22:44:39 -0700

    - Added logic to keep board same size regardless of number of items - Added Initialize Board button. TODO: eventlistener for button, prompt for size of etch board

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 const board = document.querySelector('.board');
 
-for (let i = 0; i < 16; i++){
+for (let i = 0; i < 25; i++){
     let temp = document.createElement('div');
     temp.classList.add('row');
-    for (let j = 0; j < 16; j++){
+    for (let j = 0; j < 25; j++){
         let square = document.createElement('div');
         square.classList.add('item');
         temp.appendChild(square);
@@ -18,3 +18,26 @@ square.forEach((square) => {
         square.classList.add('visited');
     });
 });
+
+function createBoard(count = 16){
+    const board = document.querySelector('.board');
+
+    for (let i = 0; i < 25; i++){
+        let temp = document.createElement('div');
+        temp.classList.add('row');
+        for (let j = 0; j < 25; j++){
+            let square = document.createElement('div');
+            square.classList.add('item');
+            temp.appendChild(square);
+        }
+        board.appendChild(temp);
+    }
+}
+
+function clearBoard(){
+    const board = document.querySelector('.board');
+
+    while(board.firstChild){
+        board.removeChild(board.firstChild);
+    }     
+}
