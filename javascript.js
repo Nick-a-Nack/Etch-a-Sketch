6db61587e40df42f5cfaf4f32f33cf8391d91f7b
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-06-16 23:10:43 -0700

    - Added prompt to button to initialize board with user input - Added input validation for isNaN and valid range on user input on button

--- javascript.js
+++ javascript.js
@@ -1,31 +1,54 @@
-const board = document.querySelector('.board');
 
-for (let i = 0; i < 25; i++){
-    let temp = document.createElement('div');
-    temp.classList.add('row');
-    for (let j = 0; j < 25; j++){
-        let square = document.createElement('div');
-        square.classList.add('item');
-        temp.appendChild(square);
-    }
-    board.appendChild(temp);
-}
+/*
+This code makes each box you mouse over a red background by giving it the visited class
+*/
 
-const square = document.querySelectorAll('.item');
+function addMouseoverListener(){
+    const square = document.querySelectorAll('.item');
 
-square.forEach((square) => {
+    square.forEach((square) => {
     square.addEventListener('mouseover', () => {
         square.classList.add('visited');
     });
+});
+}
+
+const startBoard = document.querySelector('#init');
+
+document.addEventListener('DOMContentLoaded', () => {
+    createBoard();
+    addMouseoverListener();
+});
+startBoard.addEventListener('click', () => {
+    let userChoice = prompt("How big would you like the board to be? \n (Pick between 1 and 100)");
+    
+    if (userChoice == null){
+        return;
+    }
+    while(isNaN(userChoice)){
+        alert(`${userChoice} is not a number, try again!`);
+        userChoice = prompt("How big would you like the board to be?  \n (Pick between 1 and 100)");
+    }
+
+    while (userChoice < 1 || userChoice > 100){
+        alert(`${userChoice} is not within the requested range, try again!`);
+        userChoice = prompt("How big would you like the board to be?  \n (Pick between 1 and 100)");
+    }
+
+    clearBoard();
+
+    createBoard(userChoice);
+
+    addMouseoverListener();
 });
 
 function createBoard(count = 16){
     const board = document.querySelector('.board');
 
-    for (let i = 0; i < 25; i++){
+    for (let i = 0; i < count; i++){
         let temp = document.createElement('div');
         temp.classList.add('row');
-        for (let j = 0; j < 25; j++){
+        for (let j = 0; j < count; j++){
             let square = document.createElement('div');
             square.classList.add('item');
             temp.appendChild(square);
